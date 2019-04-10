a72302a7e9fc341a2c9d665f48cb8588192da4a8
senjoshua <senjoshu@gmail.com>
2019-04-10 17:13:55 -0400

    initialized firebase

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-// var firebase = require("firebase-admin");
+var firebase = require("firebase");
+
 
 app.use(express.static(__dirname + "/public"));
     
@@ -9,3 +10,16 @@ server.listen(process.env.PORT || 8080);
 app.get("/", function(req, res){
      res.sendFile(__dirname + "/public/index.html");
 });
+
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyDf9K0vWDDZ0wYKN7sB7awYc29Ame97eh0",
+    authDomain: "web-app19.firebaseapp.com",
+    databaseURL: "https://web-app19.firebaseio.com",
+    projectId: "web-app19",
+    storageBucket: "web-app19.appspot.com",
+    messagingSenderId: "888088331447"
+  };
+  firebase.initializeApp(config);
