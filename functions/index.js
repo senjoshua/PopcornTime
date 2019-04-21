26389006c510ad901c58eb80d438468ce42e20ff
senjoshua <senjoshu@gmail.com>
2019-04-20 21:14:11 -0400

    showlist is created for each new user

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 var express = require("express");
 var app = express();
 var path = require("path");
-require("firebase/firestore");
 
 app.get('/', (req, res) => {
     let reqPath = path.join(__dirname, '../');
--- public/js/login.js
+++ public/js/login.js
@@ -13,6 +13,7 @@
   }
   checkInvalid();
   
+  var newUser = false;
   function handleSignUp() {
       var email = document.getElementById('email').value;
       var password = document.getElementById('password').value;
@@ -35,46 +36,9 @@
           alert(errorMessage);
         }
         console.log(error);
-        
-        //database
-        var db = firebase.firestore();
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            var userid = user.
-          } 
-        });
-        db.collection("users").doc("LB").set({
-          name: "Los Angeles",
-          state: "CA",
-          country: "USA"
-      })
-      .then(function() {
-          console.log("Document successfully written!");
-      })
-      .catch(function(error) {
-          console.error("Error writing document: ", error);
       });
-      //   var docData = {
-      //     stringExample: "Hello world!",
-      //     // booleanExample: true,
-      //     // numberExample: 3.14159265,
-      //     // dateExample: firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
-      //     // arrayExample: [5, true, "hello"],
-      //     // nullExample: null,
-      //     // objectExample: {
-      //     //     a: 5,
-      //     //     b: {
-      //     //         nested: "foo"
-      //     //     }
-      //     // }
-      // };
-      //   db.collection("users").doc("new-user-id").set(docData).then(function() {
-      //     console.log("Document successfully written!");
-      // 
-      // });
-
-
-      });
+      newUser = true;
+  
     }
 
     function SignIn() {
@@ -105,17 +69,35 @@
     function initApp() {
       // Listening for auth state changes
       firebase.auth().onAuthStateChanged(function(user) {
-        // if (user) {
-        //     var redirect = function(url, method) {
-        //     var form = document.createElement('form');
-        //     document.body.appendChild(form);
-        //     form.method = method;
-        //     form.action = url;
-        //     form.submit();
-        //     };
-        //     // location = '/temp.html';
-        //     redirect('/profile', 'post');
-        // } 
+        if (user) {
+            //create table for new user
+            if(newUser){
+              var db = firebase.firestore();
+              db.collection("users").doc(user.uid).collection('showlist')
+              .doc('testshow').set({
+                title: "title",
+                released: "released",
+                genre: "genre",
+                plot: "plot"
+              })
+              .then(function() {
+                  console.log("Document successfully written!");
+              })
+              .catch(function(error) {
+                  console.error("Error writing document: ", error);
+              });
+            }
+
+            var redirect = function(url, method) {
+            var form = document.createElement('form');
+            document.body.appendChild(form);
+            form.method = method;
+            form.action = url;
+            form.submit();
+            };
+            // location = '/temp.html';
+            redirect('/profile', 'post');
+        } 
         
       });
 
